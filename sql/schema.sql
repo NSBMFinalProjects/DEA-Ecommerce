ce9d4cf1bb38056789d7eecb639cbe79d1ee67d0
Dilshan Viduranga <dilshanrdv@gmail.com>
2024-04-13 22:57:27 +0530

    Added products to have many collections and vice versa

--- sql/schema.sql
+++ sql/schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS dea.delivery_details (
 CREATE INDEX IF NOT EXISTS idx_delivery_details_user_id ON dea.delivery_details (user_id);
 
 
-CREATE TABLE IF NOT EXISTS dea.admin(
+CREATE TABLE IF NOT EXISTS dea.admins(
     id ulid NOT NULL DEFAULT gen_ulid(),
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL UNIQUE,
@@ -38,5 +38,45 @@ CREATE TABLE IF NOT EXISTS dea.admin(
 
     PRIMARY KEY (id)
 );
-CREATE INDEX IF NOT EXISTS idx_admin_email ON dea.admin (email);
-CREATE INDEX IF NOT EXISTS idx_admin_username ON dea.admin (username);
+CREATE INDEX IF NOT EXISTS idx_admins_email ON dea.admins (email);
+CREATE INDEX IF NOT EXISTS idx_admins_username ON dea.admins (username);
+
+CREATE TABLE IF NOT EXISTS dea.collections (
+  id SERIAL,
+  slug VARCHAR(100) NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL,
+  photo_urls TEXT[3] NOT NULL,
+  description TEXT,
+
+  PRIMARY KEY(id)
+);
+CREATE INDEX IF NOT EXISTS idx_collections_slug ON dea.collections (slug);
+
+CREATE TABLE IF NOT EXISTS dea.products (
+  id SERIAL,
+  slug VARCHAR(100) NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL,
+  photo_urls TEXT[3] NOT NULL,
+  description TEXT,
+
+  PRIMARY KEY(id)
+);
+CREATE INDEX IF NOT EXISTS idx_products_slug ON dea.products (slug);
+
+
+CREATE TABLE IF NOT EXISTS dea.product_collections (
+  product_id INT NOT NULL,
+  collection_id INT NOT NULL,
+
+  PRIMARY KEY(product_id, collection_id),
+
+  FOREIGN KEY(product_id) REFERENCES dea.products(id),
+  CONSTRAINT fk_product_collections_product_id FOREIGN KEY (product_id)
+    REFERENCES dea.products(id),
+
+  FOREIGN KEY(collection_id) REFERENCES dea.collections(id),
+  CONSTRAINT fk_product_collections_collection_id FOREIGN KEY (collection_id)
+    REFERENCES dea.collections(id)
+);
+CREATE INDEX IF NOT EXISTS idx_products_collections_product_id ON dea.product_collections (product_id);
+CREATE INDEX IF NOT EXISTS idx_products_collections_collection_id ON dea.product_collections (collection_id);
