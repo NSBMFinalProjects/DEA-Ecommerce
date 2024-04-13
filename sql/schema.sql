b4f81f2777507d245ca4d15b0233e03d78105a2e
Dilshan Viduranga <dilshanrdv@gmail.com>
2024-04-14 01:42:14 +0530

    Added the ability to update the modified time automatically

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,18 @@
 CREATE EXTENSION IF NOT EXISTS "ulid";
 CREATE SCHEMA IF NOT EXISTS "dea";
 
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF row(NEW.*) IS DISTINCT FROM row(OLD.*) THEN
+    NEW.modified = now();
+    RETURN NEW;
+  ELSE
+    RETURN OLD;
+  END IF;
+END;
+$$ language 'plpgsql';
+
 CREATE TABLE IF NOT EXISTS dea.users (
   id ulid NOT NULL DEFAULT gen_ulid(),
   email VARCHAR(255) NOT NULL UNIQUE,
@@ -43,34 +55,67 @@ CREATE INDEX IF NOT EXISTS idx_admins_username ON dea.admins (username);
 
 CREATE TABLE IF NOT EXISTS dea.collections (
   id SERIAL,
+  created_by ulid NOT NULL,
   slug VARCHAR(100) NOT NULL UNIQUE,
   name VARCHAR(255) NOT NULL,
   photo_urls TEXT[3] NOT NULL,
   description TEXT,
+  created TIMESTAMP default now(), 
+  modified TIMESTAMP default now(),
 
-  PRIMARY KEY(id)
+  PRIMARY KEY(id),
+
+  FOREIGN KEY(created_by) REFERENCES dea.admins(id),
+  CONSTRAINT fk_collections_created_by FOREIGN KEY (created_by)
+    REFERENCES dea.admins(id)
 );
 CREATE INDEX IF NOT EXISTS idx_collections_slug ON dea.collections (slug);
+CREATE INDEX IF NOT EXISTS idx_collections_created_by ON dea.collections (created_by);
+
+DROP TRIGGER IF EXISTS update_collections_modtime on dea.collections;
+CREATE TRIGGER update_collections_modtime BEFORE UPDATE ON dea.collections FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 
 CREATE TABLE IF NOT EXISTS dea.tags (
   id SERIAL,
+  created_by ulid NOT NULL,
   slug VARCHAR(100) NOT NULL UNIQUE,
   name VARCHAR(255) NOT NULL,
+  created TIMESTAMP default now(), 
+  modified TIMESTAMP default now(),
 
-  PRIMARY KEY(id)
+  PRIMARY KEY(id),
+
+  FOREIGN KEY(created_by) REFERENCES dea.admins(id),
+  CONSTRAINT fk_tags_created_by FOREIGN KEY (created_by)
+    REFERENCES dea.admins(id)
 );
 CREATE INDEX IF NOT EXISTS idx_tags_slug ON dea.tags (slug);
+CREATE INDEX IF NOT EXISTS idx_tags_created_by ON dea.tags (created_by);
+
+DROP TRIGGER IF EXISTS update_tags_modtime on dea.tags;
+CREATE TRIGGER update_tags_modtime BEFORE UPDATE ON dea.tags FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 
 CREATE TABLE IF NOT EXISTS dea.products (
   id SERIAL,
+  created_by ulid NOT NULL,
   slug VARCHAR(100) NOT NULL UNIQUE,
   name VARCHAR(255) NOT NULL,
   photo_urls TEXT[3] NOT NULL,
   description TEXT,
+  created TIMESTAMP default now(),
+  modified TIMESTAMP default now(),
 
-  PRIMARY KEY(id)
+  PRIMARY KEY(id),
+
+  FOREIGN KEY(created_by) REFERENCES dea.admins(id),
+  CONSTRAINT fk_products_created_by FOREIGN KEY (created_by)
+    REFERENCES dea.admins(id)
 );
 CREATE INDEX IF NOT EXISTS idx_products_slug ON dea.products (slug);
+CREATE INDEX IF NOT EXISTS idx_products_created_by ON dea.products (created_by);
+
+DROP TRIGGER IF EXISTS update_products_modtime on dea.products;
+CREATE TRIGGER update_products_modtime BEFORE UPDATE ON dea.products FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 
 
 CREATE TABLE IF NOT EXISTS dea.product_collections (
@@ -109,32 +154,54 @@ CREATE INDEX IF NOT EXISTS idx_product_tags_tag_id ON dea.product_tags (tag_id);
 
 CREATE TABLE IF NOT EXISTS dea.categories (
   id SERIAL,
+  created_by ulid NOT NULL,
   product_id INT NOT NULL,
   slug VARCHAR(100) NOT NULL UNIQUE,
   name VARCHAR(100) NOT NULL,
+  created TIMESTAMP default now(),
+  modified TIMESTAMP default now(),
 
   PRIMARY KEY(id),
 
   FOREIGN KEY(product_id) REFERENCES dea.products(id),
   CONSTRAINT fk_categories_product_id FOREIGN KEY (product_id)
-    REFERENCES dea.products(id)
+    REFERENCES dea.products(id),
+
+  FOREIGN KEY(created_by) REFERENCES dea.admins(id),
+  CONSTRAINT fk_categories_created_by FOREIGN KEY (created_by)
+    REFERENCES dea.admins(id)
 );
 CREATE INDEX IF NOT EXISTS idx_categories_slug ON dea.categories (slug);
 CREATE INDEX IF NOT EXISTS idx_categories_product_id ON dea.categories (product_id);
+CREATE INDEX IF NOT EXISTS idx_categories_created_by ON dea.categories (created_by);
+
+DROP TRIGGER IF EXISTS update_categories_modtime on dea.categories;
+CREATE TRIGGER update_categories_modtime BEFORE UPDATE ON dea.categories FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 
 CREATE TABLE IF NOT EXISTS dea.colors (
   id SERIAL,
   category_id INT NOT NULL,
+  created_by ulid NOT NULL,
   slug VARCHAR(100) NOT NULL UNIQUE,
   name VARCHAR(100) NOT null,
   hex VARCHAR(100) NOT NULL,
   qty INT NOT NULL,
+  created TIMESTAMP default now(),
+  modified TIMESTAMP default now(),
 
   PRIMARY KEY(id),
 
   FOREIGN KEY(category_id) REFERENCES dea.categories(id),
   CONSTRAINT fk_colors_category_id FOREIGN KEY (category_id)
-    REFERENCES dea.categories(id)
+    REFERENCES dea.categories(id),
+
+  FOREIGN KEY(created_by) REFERENCES dea.admins(id),
+  CONSTRAINT fk_colors_created_by FOREIGN KEY (created_by)
+    REFERENCES dea.admins(id)
 );
 CREATE INDEX IF NOT EXISTS idx_colors_slug ON dea.colors (slug);
-CREATE INDEX IF NOT EXISTS idx_colors_category_id ON dea.colors (category_id)
+CREATE INDEX IF NOT EXISTS idx_colors_category_id ON dea.colors (category_id);
+CREATE INDEX IF NOT EXISTS idx_colors_created_by ON dea.colors (created_by);
+
+DROP TRIGGER IF EXISTS update_colors_modtime on dea.colors;
+CREATE TRIGGER update_colors_modtime BEFORE UPDATE ON dea.colors FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
