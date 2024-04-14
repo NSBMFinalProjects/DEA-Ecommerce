79173a3a9929f4d0b4f4888b2044099fdfd786c2
Dilshan Viduranga <dilshanrdv@gmail.com>
2024-04-14 11:48:29 +0530

    Modified the user model to match the schema

--- admin/src/main/java/nsbm/dea/admin/model/User.java
+++ admin/src/main/java/nsbm/dea/admin/model/User.java
@@ -3,15 +3,27 @@ package nsbm.dea.admin.model;
 public class User {
   private String id;
   private String email;
+  private String username;
+  private String name;
   private String password;
+  private String photoURL;
 
   public User() {
   }
 
-  public User(String id, String email, String password) {
+  public User(
+      String id,
+      String email,
+      String username,
+      String name,
+      String password,
+      String photo_url) {
     this.id = id;
     this.email = email;
+    this.username = username;
+    this.name = name;
     this.password = password;
+    this.photoURL = photo_url;
   }
 
   public String getId() {
@@ -30,11 +42,35 @@ public class User {
     this.email = email;
   }
 
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public String getPassword() {
     return password;
   }
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public String getPhotoURL() {
+    return photoURL;
+  }
+
+  public void setPhotoURL(String photoURL) {
+    this.photoURL = photoURL;
   }
 }
