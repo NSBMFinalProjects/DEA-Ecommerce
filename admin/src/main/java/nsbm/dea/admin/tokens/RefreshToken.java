3f5c468d6874649e7429cb58ddedf6ae6975de40
Dilshan Viduranga <dilshanrdv@gmail.com>
2024-04-16 22:35:22 +0530

    Added Refresh tokens

---
+++ admin/src/main/java/nsbm/dea/admin/tokens/RefreshToken.java
@@ -0,0 +1,104 @@
+package nsbm.dea.admin.tokens;
+
+import java.time.Clock;
+import java.time.Instant;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.github.f4b6a3.ulid.Ulid;
+import com.github.f4b6a3.ulid.UlidCreator;
+
+import nsbm.dea.admin.config.Env;
+import nsbm.dea.admin.connections.Redis;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class RefreshToken {
+  private static final Algorithm algorithm = Algorithm.HMAC256(Env.getRefreshTokenSecret());
+  private static final String iss = "dea";
+
+  private long iat;
+  private long nbf;
+  private long exp;
+  private String ulid;
+  private String sub;
+
+  private String getKeyForRedis(String key) {
+    return String.format("admin_refresh_token-%s", key);
+  }
+
+  public String generate(String id) {
+    long now = Instant.now(Clock.systemUTC()).getEpochSecond();
+    Ulid ulid = UlidCreator.getUlid();
+
+    this.iat = now;
+    this.nbf = now;
+    this.exp = now + Env.getRefreshTokenExp();
+    this.ulid = ulid.toString();
+    this.sub = id;
+
+    try (JedisPool pool = Redis.getPool()) {
+      try (Jedis jedis = pool.getResource()) {
+        jedis.setex(this.getKeyForRedis(this.ulid), Env.getRefreshTokenExp(), "default");
+      }
+    }
+
+    return JWT.create().withIssuer(RefreshToken.iss).withClaim("sub", this.sub).withClaim("token_id", this.ulid)
+        .withClaim("iat", this.iat)
+        .withClaim("nbf", this.nbf)
+        .withClaim("exp", this.exp)
+        .sign(algorithm);
+  }
+
+  public boolean isValid(String token, String id) {
+    try {
+      JWTVerifier verifier = JWT.require(algorithm).withIssuer(RefreshToken.iss).withClaim("sub", id)
+          .build();
+
+      DecodedJWT jwt = verifier.verify(token);
+      this.ulid = jwt.getClaim("token_id").asString();
+      this.sub = jwt.getClaim("sub").asString();
+
+      try (JedisPool pool = Redis.getPool()) {
+        try (Jedis jedis = pool.getResource()) {
+          String value = jedis.get(this.getKeyForRedis(this.ulid));
+          if (value == null) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    } catch (JWTVerificationException e) {
+      System.err.println(e.getMessage());
+      return false;
+    }
+  }
+
+  public static String getIss() {
+    return iss;
+  }
+
+  public long getIat() {
+    return iat;
+  }
+
+  public long getNbf() {
+    return nbf;
+  }
+
+  public long getExp() {
+    return exp;
+  }
+
+  public String getUlid() {
+    return ulid;
+  }
+
+  public String getSub() {
+    return sub;
+  }
+}
