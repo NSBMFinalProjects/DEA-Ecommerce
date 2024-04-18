8de5997b22cfbd6d82ea05d2e5561e94616efc61
Dilshan Viduranga <dilshanrdv@gmail.com>
2024-04-18 18:02:05 +0530

    Minor fix

--- admin/src/main/java/nsbm/dea/admin/dao/ProductDAO.java
+++ admin/src/main/java/nsbm/dea/admin/dao/ProductDAO.java
@@ -6,92 +6,93 @@ import nsbm.dea.admin.model.Product;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-
 
 public class ProductDAO {
 
-    public ProductDAO(){
+  public ProductDAO() {
 
+  }
+
+  public List<Product> getAllProducts() throws SQLException {
+    List<Product> products = new ArrayList<Product>();
+
+    try (Connection connection = DB.getConnection()) {
+      PreparedStatement statement = connection.prepareStatement("select * from dea.products");
+      ResultSet resultSet = statement.executeQuery();
+
+      while (resultSet.next()) {
+        Product product = new Product();
+        product.setId(resultSet.getInt("id"));
+        product.setName(resultSet.getString("name"));
+        product.setDescription(resultSet.getString("description"));
+        product.setSlug(resultSet.getString("slug"));
+        product.setCreatedBy(resultSet.getString("created by"));
+
+        String photoUrl = resultSet.getString("photo_url");
+        String[] photoUrls = photoUrl.split(",");
+
+        product.setPhotoUrls(photoUrls);
+        product.setCreated(Timestamp.from(resultSet.getTimestamp("created").toInstant()));
+        product.setModified(Timestamp.from(resultSet.getTimestamp("modified").toInstant()));
+
+        products.add(product);
+      }
+
+    } catch (SQLException e) {
+      throw new SQLException(e.getMessage());
     }
+    return products;
+  }
 
-    public List<Product> getAllProducts() throws SQLException{
-        List<Product> products = new ArrayList<Product>();
+  public int addProduct(Product product) throws SQLException {
+    try (Connection connection = DB.getConnection()) {
+      String sql = "insert into dea.products(created_by, name, photo_urls, description) values(CAST(? AS ulid),?,?,?) returning id";
+      try (PreparedStatement statement = connection.prepareStatement(sql)) {
+        statement.setString(1, product.getCreatedBy());
+        statement.setString(2, product.getName());
+        statement.setArray(3, connection.createArrayOf("text", product.getPhotoUrls()));
+        statement.setString(4, product.getDescription());
+        try (ResultSet resultSet = statement.executeQuery()) {
+          if (resultSet.next()) {
+            return resultSet.getInt("id");
+          }
 
-        try (Connection connection= DB.getConnection()){
-            PreparedStatement statement=connection.prepareStatement("select * from dea.products");
-            ResultSet resultSet=statement.executeQuery();
-
-            while(resultSet.next()){
-                Product product=new Product();
-                product.setId(resultSet.getInt("id"));
-                product.setName(resultSet.getString("name"));
-                product.setDescription(resultSet.getString("description"));
-                product.setSlug(resultSet.getString("slug"));
-                product.setCreatedBy(resultSet.getString("created by"));
-
-                String photoUrl=resultSet.getString("photo_url");
-                String[] photoUrls=photoUrl.split(",");
-
-                product.setPhotoUrls(photoUrls);
-                product.setCreated(Timestamp.from(resultSet.getTimestamp("created").toInstant()));
-                product.setModified(Timestamp.from(resultSet.getTimestamp("modified").toInstant()));
-
-                products.add(product);
-            }
-
-        }catch (SQLException e){
-            throw new SQLException(e.getMessage());
+          throw new SQLException("failed to create product");
         }
-        return products;
+      }
     }
+  }
 
-    public boolean addProduct(Product product) throws SQLException{
-        try (Connection connection= DB.getConnection()){
-            String createdBy= "01HVJJBZNAAXKBT6KA816QW41N";
-            String sql="insert into dea.products(created_by, name, photo_urls, description) values(?,?,?,?)";
-            try(PreparedStatement statement=connection.prepareStatement(sql)) {
-                statement.setObject(1,createdBy );
-                statement.setString(2, product.getName());
-                statement.setArray(3, connection.createArrayOf("text", product.getPhotoUrls()));
-                statement.setString(4, product.getDescription());
-                int rowsAffected=statement.executeUpdate();
-                 return rowsAffected>0;
-            }
-        }
-        catch (SQLException e){
-            throw new SQLException(e.getMessage());
-        }
+  public boolean updateProduct(Product product) throws SQLException {
+
+    try (Connection connection = DB.getConnection()) {
+      String sql = "update dea.products set name=?,description=?,photo_urls=? where id=?";
+      PreparedStatement statement = connection.prepareStatement(sql);
+
+      statement.setString(1, product.getName());
+      statement.setString(2, product.getDescription());
+      statement.setString(3, String.join(",", product.getPhotoUrls()));
+
+      int updateResult = statement.executeUpdate();
+      return updateResult > 0;
+
+    } catch (SQLException e) {
+      throw new SQLException(e.getMessage());
     }
+  }
 
-    public boolean updateProduct(Product product) throws SQLException{
+  public boolean deleteProduct(Product product) throws SQLException {
+    try (Connection connection = DB.getConnection()) {
+      String sql = "delete from dea.products where id=?";
+      PreparedStatement statement = connection.prepareStatement(sql);
+      statement.setInt(1, product.getId());
+      int result = statement.executeUpdate();
 
-        try (Connection connection= DB.getConnection()){
-            String sql="update dea.products set name=?,description=?,photo_urls=? where id=?";
-            PreparedStatement statement=connection.prepareStatement(sql);
-
-            statement.setString(1,product.getName());
-            statement.setString(2,product.getDescription());
-            statement.setString(3, String.join(",", product.getPhotoUrls()));
-
-            int updateResult=statement.executeUpdate();
-            return updateResult>0;
-
-        }catch (SQLException e){
-            throw new SQLException(e.getMessage());
-        }
+      return result > 0;
+    } catch (SQLException e) {
+      throw new SQLException(e.getMessage());
     }
-
-    public boolean deleteProduct(Product product) throws SQLException{
-        try (Connection connection= DB.getConnection()){
-            String sql="delete from dea.products where id=?";
-            PreparedStatement statement=connection.prepareStatement(sql);
-            statement.setInt(1,product.getId());
-            int result=statement.executeUpdate();
-
-            return result>0;
-        }catch (SQLException e){
-            throw new SQLException(e.getMessage());
-        }
-    }
+  }
 }
--- admin/src/main/java/nsbm/dea/admin/servlets/products/Create.java
+++ admin/src/main/java/nsbm/dea/admin/servlets/products/Create.java
@@ -1,78 +1,68 @@
 package nsbm.dea.admin.servlets.products;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nsbm.dea.admin.dao.CategoryDAO;
+import nsbm.dea.admin.dao.ProductDAO;
 import nsbm.dea.admin.enums.Status;
 import nsbm.dea.admin.lib.Lib;
 import nsbm.dea.admin.model.Product;
-import nsbm.dea.admin.dao.ProductDAO;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-@WebServlet(name = "product_create",value = "/product/create")
+@WebServlet(name = "product_create", value = "/product/create")
 public class Create extends HttpServlet {
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    try {
+      response.setContentType("application/json");
+      JsonObject object = Lib.getJSONPayloadFromRequest(request);
 
-    private final ProductDAO productDAO;
-    private final CategoryDAO categoryDAO;
+      String adminId = "01HVJJBZNAAXKBT6KA816QW41N";
 
-    public Create(ProductDAO productDAO, CategoryDAO categoryDAO) {
-        this.productDAO = productDAO;
-        this.categoryDAO = categoryDAO;
+      String name = object.get("name").getAsString();
+      String description = object.get("description").getAsString();
+      JsonArray photos = object.getAsJsonArray("photos");
+      List<String> photoUrls = new ArrayList<>();
+      for (JsonElement photo : photos) {
+        String photoName = photo.getAsString();
+        photoUrls.add(photoName);
+      }
 
+      String[] photoUrlArray = photoUrls.toArray(new String[0]);
+
+      Product product = new Product();
+      product.setName(name);
+      product.setDescription(description);
+      product.setPhotoUrls(photoUrlArray);
+      product.setCreatedBy(adminId);
+
+      ProductDAO productDAO = new ProductDAO();
+      int productId = productDAO.addProduct(product);
+      System.out.println(productId);
+
+      Lib.sendJSONResponse(response, HttpServletResponse.SC_OK, Status.OK, "created the product  sucessfully");
+      // JsonObject sizesObject = object.getAsJsonObject("sizes");
+      // for (String size : sizesObject.keySet()) {
+      // Categories newCategory=new Categories();
+      // newCategory.setName(size);
+      // newCategory.setCreatedBy(createBy);
+      // newCategory.setProductId(produt);
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+      // Handle error
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try{
-           response.setContentType("application/json");
-           JsonObject object=Lib.getJSONPayloadFromRequest(request);
-
-
-           String name = object.get("name").getAsString();
-           String description = object.get("description").getAsString();
-           JsonArray photos=object.getAsJsonArray("photos");
-           List<String> photoUrls=new ArrayList<>();
-           for(JsonElement photo:photos){
-               String photoName=photo.getAsString();
-                photoUrls.add(photoName);
-           }
-
-           String[] photoUrlArray=photoUrls.toArray(new String[0]);
-
-           Product product=new Product();
-           product.setName(name);
-           product.setDescription(description);
-           product.setPhotoUrls(photoUrlArray);
-
-
-           boolean isProductAdded=productDAO.addProduct(product);
-
-
-           if(isProductAdded){
-               Lib.sendJSONResponse(response, HttpServletResponse.SC_OK, Status.OK, "created the product  sucessfully");
-//               JsonObject sizesObject = object.getAsJsonObject("sizes");
-//               for (String size : sizesObject.keySet()) {
-//                   Categories newCategory=new Categories();
-//                   newCategory.setName(size);
-//                   newCategory.setCreatedBy(createBy);
-//                   newCategory.setProductId(produt);
-//
-//               }
-           }
-
-
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-    }
+  }
 }
