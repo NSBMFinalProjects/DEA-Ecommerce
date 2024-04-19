bb58bad614b76f495f0fb1d0dc31aff0a4e209be
Ravindu Sulakshana <ravindusulakshana2525@gmail.com>
2024-04-19 12:30:26 +0530

    Added refresh token

---
+++ web/src/main/java/nsbm/dea/web/tokens/RefreshToken.java
@@ -0,0 +1,142 @@
+package nsbm.dea.web.tokens;
+
+import java.time.Clock;
+import java.time.Instant;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.github.f4b6a3.ulid.UlidCreator;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import nsbm.dea.web.config.Env;
+import nsbm.dea.web.connections.Redis;
+import nsbm.dea.web.errors.UnauthorizedException;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class RefreshToken {
+  private static final Algorithm algorithm = Algorithm.HMAC256(Env.getRefreshTokenSecret());
+  private static final String iss = "dea";
+
+  private long iat;
+  private long nbf;
+  private long exp;
+  private String ulid;
+  private String sub;
+
+  private String token;
+
+  public static String getKeyForRedis(String key) {
+    return String.format("web_refresh_token-%s", key);
+  }
+
+  public String generate(String id) {
+    long now = Instant.now(Clock.systemUTC()).getEpochSecond();
+
+    this.ulid = UlidCreator.getUlid().toString();
+    this.iat = now;
+    this.nbf = now;
+    this.exp = now + Env.getRefreshTokenExp();
+    this.sub = id;
+
+    try (JedisPool pool = Redis.getPool()) {
+      try (Jedis jedis = pool.getResource()) {
+        jedis.setex(RefreshToken.getKeyForRedis(this.ulid), Env.getRefreshTokenExp(), "default");
+      }
+    }
+
+    this.token = JWT.create().withIssuer(RefreshToken.iss).withClaim("sub", this.sub).withClaim("token_id", this.ulid)
+        .withClaim("iat", this.iat)
+        .withClaim("nbf", this.nbf)
+        .withClaim("exp", this.exp)
+        .sign(algorithm);
+    return this.token;
+  }
+
+  public boolean isValid(String token) {
+    try {
+      JWTVerifier verifier = JWT.require(algorithm).withIssuer(RefreshToken.iss).build();
+
+      DecodedJWT jwt = verifier.verify(token);
+      this.ulid = jwt.getClaim("token_id").asString();
+      this.sub = jwt.getClaim("sub").asString();
+
+      try (JedisPool pool = Redis.getPool()) {
+        try (Jedis jedis = pool.getResource()) {
+          String value = jedis.get(RefreshToken.getKeyForRedis(this.ulid));
+          if (value == null) {
+            return false;
+          }
+        }
+      }
+
+      this.token = token;
+      return true;
+    } catch (JWTVerificationException e) {
+      System.err.println(e.getMessage());
+      return false;
+    }
+  }
+
+  public void delete(String token) throws UnauthorizedException {
+    if (!this.isValid(token)) {
+      throw new UnauthorizedException("refresh token is not valid");
+    }
+
+    try (JedisPool pool = Redis.getPool()) {
+      try (Jedis jedis = pool.getResource()) {
+        String rtk = jedis.get(RefreshToken.getKeyForRedis(this.ulid));
+        if (rtk == null) {
+          throw new UnauthorizedException("refresh token is not valid");
+        }
+        if (rtk != "default") {
+          jedis.del(rtk);
+        }
+        jedis.del(RefreshToken.getKeyForRedis(this.ulid));
+      }
+    }
+  }
+
+  public void cookie(HttpServletResponse response) {
+    Cookie cookie = new Cookie("refresh_token", this.token);
+    cookie.setMaxAge(Math.toIntExact(Env.getRefreshTokenExp()));
+    cookie.setDomain(Env.getDomain());
+    cookie.setPath("/");
+    cookie.setSecure(Env.getEnv() == "PROD" ? false : true);
+    cookie.setHttpOnly(true);
+
+    response.addCookie(cookie);
+  }
+
+  public static String getIss() {
+    return iss;
+  }
+
+  public long getIat() {
+    return iat;
+  }
+
+  public long getNbf() {
+    return nbf;
+  }
+
+  public long getExp() {
+    return exp;
+  }
+
+  public String getUlid() {
+    return ulid;
+  }
+
+  public String getSub() {
+    return sub;
+  }
+
+  public String getToken() {
+    return token;
+  }
+}
