f237d15fc0dbcc0f23e877ce19d3669116240ebf
Ravindu Sulakshana <ravindusulakshana2525@gmail.com>
2024-04-19 09:36:31 +0530

    Added the Env file to load env variables

---
+++ web/src/main/java/nsbm/dea/web/config/Env.java
@@ -0,0 +1,55 @@
+package nsbm.dea.web.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Env {
+  private static final Dotenv dotenv = Dotenv.load();
+
+  public static String getDBURL() {
+    return dotenv.get("DB_URL");
+  }
+
+  public static String getDBUser() {
+    return dotenv.get("DB_USER");
+  }
+
+  public static String getDBPassword() {
+    return dotenv.get("DB_PASSWWORD");
+  }
+
+  public static String getRefreshTokenSecret() {
+    return dotenv.get("REFRESH_TOKEN_SECRET");
+  }
+
+  public static long getRefreshTokenExp() {
+    return Long.parseLong(dotenv.get("REFRESH_TOKEN_EXP"));
+  }
+
+  public static String getAccessTokenSecret() {
+    return dotenv.get("ACCESS_TOKEN_SECRET");
+  }
+
+  public static long getAccessTokenExp() {
+    return Long.parseLong(dotenv.get("ACCESS_TOKEN_EXP"));
+  }
+
+  public static String getSessionTokenSecret() {
+    return dotenv.get("SESSION_TOKEN_SECRET");
+  }
+
+  public static long getSessionTokenExp() {
+    return Long.parseLong(dotenv.get("SESSION_TOKEN_EXP"));
+  }
+
+  public static String getRedisURL() {
+    return dotenv.get("REDIS_URL");
+  }
+
+  public static String getEnv() {
+    return dotenv.get("ENV");
+  }
+
+  public static String getDomain() {
+    return dotenv.get("DOMAIN");
+  }
+}
