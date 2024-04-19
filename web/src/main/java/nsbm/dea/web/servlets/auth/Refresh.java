384bd2902d38ac46154ceb6788d508cbdba0f8e1
Ravindu Sulakshana <ravindusulakshana2525@gmail.com>
2024-04-19 17:53:01 +0530

    Added the ability to refresh access tokens

---
+++ web/src/main/java/nsbm/dea/web/servlets/auth/Refresh.java
@@ -0,0 +1,56 @@
+package nsbm.dea.web.servlets.auth;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nsbm.dea.web.enums.Status;
+import nsbm.dea.web.errors.UnauthorizedException;
+import nsbm.dea.web.lib.Auth;
+import nsbm.dea.web.lib.Lib;
+import nsbm.dea.web.tokens.AccessToken;
+import nsbm.dea.web.tokens.RefreshToken;
+
+@WebServlet(name = "refresh", value = "/auth/refresh")
+public class Refresh extends HttpServlet {
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    try {
+      String userId = "01HVKE6TZGM8F75Z1YX2R5XNHE";
+
+      Optional<Cookie> rto = Lib.getCookieByName(request, "refresh_token");
+      if (rto.isEmpty()) {
+        throw new UnauthorizedException("refresh token is not present");
+      }
+      String rtv = rto.get().getValue();
+
+      RefreshToken rt = new RefreshToken();
+      if (!rt.isValid(rtv)) {
+        throw new UnauthorizedException("refresh token is not valid");
+      }
+
+      AccessToken at = new AccessToken();
+      at.generate(userId, rt.getUlid());
+      at.cookie(response);
+
+      Lib.sendJSONResponse(response, HttpServletResponse.SC_OK, Status.OK, "token refreshed");
+      return;
+    } catch (UnauthorizedException e) {
+      System.err.println(e.getMessage());
+      Auth.removeAuthCookies(request, response);
+      Lib.sendJSONResponse(response, HttpServletResponse.SC_UNAUTHORIZED, Status.UNAUTHORIZED,
+          "you are not logged in login to proceed");
+      return;
+    } catch (Exception e) {
+      e.printStackTrace();
+      Lib.sendJSONResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR,
+          "something went wrong");
+      return;
+    }
+  }
+}
