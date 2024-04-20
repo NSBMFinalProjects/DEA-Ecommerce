e8d6e840167eb1566902f516148a4f4e3bacc8b2
Ravindu Sulakshana <ravindusulakshana2525@gmail.com>
2024-04-21 02:56:41 +0530

    Added DAO to get product with specific id

--- admin/src/main/java/nsbm/dea/admin/dao/ProductDAO.java
+++ admin/src/main/java/nsbm/dea/admin/dao/ProductDAO.java
@@ -6,12 +6,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import nsbm.dea.admin.connections.DB;
+import nsbm.dea.admin.model.Category;
+import nsbm.dea.admin.model.Color;
 import nsbm.dea.admin.model.Product;
 
 public class ProductDAO {
   private int LIMIT;
+  private static final Gson gson = new Gson();
 
   public ProductDAO() {
   }
@@ -30,6 +40,105 @@ public class ProductDAO {
         resultSet.getString("description"),
         resultSet.getTimestamp("created"),
         resultSet.getTimestamp("modified"));
+  }
+
+  public Optional<Product> getProductById(int id) throws SQLException {
+    String sql = """
+        SELECT
+        	jsonb_build_object ('product',
+        		jsonb_build_object ('id',
+        			products.id,
+        			'name',
+        			products.name,
+        			'description',
+        			products.description,
+        			'slug',
+        			products.slug,
+        			'photo_urls',
+        			products.photo_urls,
+        			'created_by',
+        			products.created_by,
+        			'created',
+        			products.created,
+        			'modified',
+        			products.modified,
+        			'categories',
+        			(
+        				SELECT
+        					jsonb_agg(jsonb_build_object ('id', categories.id, 'name', categories.name, 'slug', categories.slug, 'created_by', categories.created_by, 'created', categories.created, 'modified', categories.modified, 'colors', (
+        								SELECT
+        									jsonb_agg(jsonb_build_object ('id', colors.id, 'name', colors.name, 'slug', colors.slug, 'hex', colors.hex, 'qty', colors.qty, 'price', colors.price, 'created_by', colors.created_by, 'created', colors.created, 'modified', colors.modified))
+        									FROM dea.colors colors
+        								WHERE
+        									colors.category_id = categories.id)))
+        				FROM
+        					dea.categories categories
+        				WHERE
+        					categories.product_id = products.id))) AS data
+        	FROM
+        		dea.products products
+        	WHERE
+        		products.id = ?
+        	LIMIT 1;
+                                       """;
+
+    JsonObject productJson;
+
+    try (Connection connection = DB.getConnection()) {
+      try (PreparedStatement statement = connection.prepareStatement(sql)) {
+        statement.setInt(1, id);
+        try (ResultSet resultSet = statement.executeQuery()) {
+          if (!resultSet.next()) {
+            return Optional.empty();
+          }
+
+          JsonObject payload = JsonParser.parseString(resultSet.getString("data")).getAsJsonObject();
+          productJson = payload.getAsJsonObject("product");
+        }
+      }
+    }
+
+    JsonArray categoriesJsonArray = productJson.getAsJsonArray("categories");
+    List<Category> categories = new ArrayList<>();
+    for (JsonElement categoryElement : categoriesJsonArray) {
+      JsonObject categoryJson = categoryElement.getAsJsonObject();
+
+      JsonArray colorsJsonArray = categoryJson.getAsJsonArray("colors");
+      List<Color> colors = new ArrayList<>();
+      for (JsonElement colorElement : colorsJsonArray) {
+        JsonObject colorJson = colorElement.getAsJsonObject();
+        Color color = new Color();
+        color.setId(colorJson.get("id").getAsInt());
+        color.setName(colorJson.get("name").getAsString());
+        color.setSlug(colorJson.get("slug").getAsString());
+        color.setHex(colorJson.get("hex").getAsString());
+        color.setQuantity(colorJson.get("qty").getAsInt());
+        color.setPrice(colorJson.get("price").getAsBigDecimal());
+        color.setCreatedBy(colorJson.get("created_by").getAsString());
+
+        colors.add(color);
+      }
+
+      Category category = new Category(
+          categoryJson.get("id").getAsInt(),
+          categoryJson.get("created_by").getAsString(),
+          categoryJson.get("slug").getAsString(),
+          categoryJson.get("name").getAsString(),
+          colors.stream().toArray(Color[]::new));
+
+      categories.add(category);
+    }
+
+    Product product = new Product(
+        productJson.get("id").getAsInt(),
+        productJson.get("created_by").getAsString(),
+        productJson.get("slug").getAsString(),
+        productJson.get("name").getAsString(),
+        gson.fromJson(productJson.get("photo_urls"), String[].class),
+        productJson.get("description").getAsString(),
+        categories.stream().toArray(Category[]::new));
+
+    return Optional.of(product);
   }
 
   public List<Product> getAllProducts(int page) throws SQLException {
